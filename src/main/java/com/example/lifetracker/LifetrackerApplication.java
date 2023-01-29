48566a031401ff8cf7a554d4a190b63035672110
RexBearIU <rexbear87941@gmail.com>
2023-01-29 16:12:58 +0800

    firsttest

---
+++ src/main/java/com/example/lifetracker/LifetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lifetracker/LifetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
